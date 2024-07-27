a54cefae4066c6ec4b61c0f018c25920eea75cb1
Neercshsh <neersqueen143@gmail.com>
2024-07-27 10:10:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7499687967:AAGkCqDENOJQbXOKtpOYvqvFEkh9_UR99iE")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6404702674")
 
 
 
